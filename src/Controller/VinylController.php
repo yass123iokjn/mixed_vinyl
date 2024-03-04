5678d9dae62bdb8fc0cb50211cedf911b618684d
yasmine mansour <yasminemansour2003@gmail.com>
2024-03-05 00:32:03 +0100

    route -> controller -> response -> profit

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+class VinylController
+{
+    #[Route('')]
+    public function homepage(): Response
+    {
+        return new Response('Title :PB and');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+        return new Response($title);
+    }
+}
